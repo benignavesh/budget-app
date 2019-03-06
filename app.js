6b87baa074f8659b8ebabca060109d57633f64e8
Avesh Verma <avesh.169105057@muj.manipal.edu>
2019-03-06 10:54:34 +0530

    init fix

--- app.js
+++ app.js
@@ -238,7 +238,14 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
             setupEventListeners();
+            
         }
     }
 })(budgetController, UIController);
